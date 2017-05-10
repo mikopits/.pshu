5429f694be858d121fc806a65746ff6ef4995752
mikopits <mikopits@gmail.com>
2017-05-09 17:46:08 -0700

    Move to base16 solarflare color scheme. Update zprezto and tmux conf.

--- .pshu/zsh/term.zsh
+++ .pshu/zsh/term.zsh
@@ -1,5 +1,14 @@
+# Allow 256 colours if term allows
 if [ -e /usr/share/terminfo/x/xterm-256color ]; then
-        export TERM='xterm-256color'
+    export TERM='xterm-256color'
 else
-        export TERM='xterm-color'
+    export TERM='xterm-color'
 fi
+
+# Configure base16 for shell
+BASE16_SHELL=$HOME/.config/base16-shell
+[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+
+# Choose a base16 theme
+BASE16_THEME='solarflare'
+source "$BASE16_SHELL/scripts/base16-$BASE16_THEME.sh"
