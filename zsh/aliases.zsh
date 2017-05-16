23cbb576016a8e0ac25286439345514cee1b8aa4
mikopits <mikopits@gmail.com>
2017-05-16 12:04:46 +0000

    Update directories in aliases.zsh to match new directory structure.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,5 @@
 # Shortcut to the dotfile root
-pshu=$HOME/.pshu/.pshu
+pshu=$HOME/.pshu
 
 # Edit and reload aliases
 alias ae='vim $pshu/zsh/aliases.zsh'
@@ -23,15 +23,12 @@ alias v='fasd -f -e vim'    # Quick opening files with vim
 # Install/Update/Clean Vim plugins
 alias vimu='vim +PluginInstall +qall'
 alias vimc='vim +PluginClean +qall'
-alias vimp='vim ~/.pshu/vim/vundles/plugins.vundle'
+alias vimp='vim $pshu/vim/vundles/plugins.vundle'
 
 # Rust Cargo shortcuts
 alias crun='RUST_BACKTRACE=1 cargo run'
 alias cbuild='RUST_BACKTRACE=1 cargo build'
 alias ctest='RUST_BACKTRACE=1 cargo test'
-
-# Update boot params
-alias update-grub='grub-mkconfig -o /boot/grub/grub.cfg'
 
 # xsel shortcuts
 alias pbcopy='xsel --clipboard --input'
