4138aea288a6e59a0b6c5c227e874a15782b1893
pshuu <pshu@protonmail.com>
2017-05-19 23:16:27 +0000

    Add configuration file for term - default to xterm unless spcified otherwise

--- zsh/term.zsh
+++ zsh/term.zsh
@@ -1,19 +1,31 @@
-# Use 256 Colour xterm if term allows
-if [ -e /usr/share/terminfo/t/tmux-256color ]; then
-    export TERM='tmux-256color'
-else
-    # Try to download the terminfo
-    echo "Did not find tmux-256color. Need sudo to install..."
-    sudo curl https://mikopits.github.io/public/tmux-256color -o /usr/share/terminfo/t/tmux-256color
+# Read from the terminal config
+source $HOME/.pshu/config/term.cfg
+
+if [ $term == "tmux-256color" ]; then
+    # Use 256 Colour xterm if term allows
     if [ -e /usr/share/terminfo/t/tmux-256color ]; then
         export TERM='tmux-256color'
     else
-        # Failed to get the terminfo, fall back to xterm
-        if [ -e /usr/share/terminfo/x/xterm-256color ]; then
-            export TERM='xterm-256color'
+        # Try to download the terminfo
+        echo "Did not find tmux-256color. Need sudo to install..."
+        sudo curl https://mikopits.github.io/public/tmux-256color -o /usr/share/terminfo/t/tmux-256color
+        if [ -e /usr/share/terminfo/t/tmux-256color ]; then
+            export TERM='tmux-256color'
         else
-            export TERM='xterm-color'
+            # Failed to get the terminfo, fall back to xterm
+            if [ -e /usr/share/terminfo/x/xterm-256color ]; then
+                export TERM='xterm-256color'
+            else
+                export TERM='xterm-color'
+            fi
         fi
+    fi
+else
+    # Default to xterm
+    if [ -e /usr/share/terminfo/x/xterm-256color ]; then
+        export TERM='xterm-256color'
+    else
+        export TERM='xterm-color'
     fi
 fi
 
